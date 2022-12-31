9b15742cf35def5d007606b1351e073012620b77
Vasyl Yakymets <Vasia911119@gmail.com>
2022-12-31 14:30:18 +0200

    fix

--- src/js/modal-renderer.js
+++ src/js/modal-renderer.js
@@ -1,23 +1,19 @@
 import filmCardTemplate from '../templates/modalFilmTemplate.hbs';
 
-class ModalFilmRenderer
-{
-    #modalFilDetailContainer;
+class ModalFilmRenderer {
+  modalFilDetailContainer;
 
-    constructor()
-    {
-        this.#modalFilDetailContainer = document.querySelector('.film-detail');
-    }
+  constructor() {
+    this.modalFilDetailContainer = document.querySelector('.film-detail');
+  }
 
-    render(data)
-    {
-        this.#modalFilDetailContainer.insertAdjacentHTML('beforeend', filmCardTemplate(data));
-    }
+  render(data) {
+    this.modalFilDetailContainer.insertAdjacentHTML('beforeend', filmCardTemplate(data));
+  }
 
-    clear()
-    {
-        this.#modalFilDetailContainer.innerHTML = '';
-    }
+  clear() {
+    this.modalFilDetailContainer.innerHTML = '';
+  }
 }
 
 export default new ModalFilmRenderer();
